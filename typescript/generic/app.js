9a84d392c919edaf6d1c0463f101d9de6646f743
anonymouscode7870 <anonymouscoder8910@gmail.com>
2026-05-18 12:59:19 +0530

    generic

---
+++ typescript/generic/app.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function abcd(val) {
+    console.log(val);
+}
+abcd("abcd");
+// ..............................................................
+function pqrs(val1, val2) {
+    console.log(val1);
+    console.log(val2);
+}
+pqrs("pqrs", 23);
+//# sourceMappingURL=app.js.map
---
+++ typescript/generic/app.ts
@@ -0,0 +1,13 @@
+function abcd<T>(val:T): void {
+    console.log(val);
+}
+
+abcd<string>("abcd");
+
+
+// ..............................................................
+function pqrs<T, U>(val1:T, val2:U): void {
+    console.log(val1);
+    console.log(val2);
+}
+pqrs<string, number>("pqrs", 23);
